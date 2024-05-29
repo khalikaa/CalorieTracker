a783ca4450b79e13d1346b6a6d45c00c1651d37f
khalikaa <khalikaaaaa@gmail.com>
2024-05-29 15:41:24 +0800

    feat: Create new Model NutriInfo

---
+++ CalorieTracker/app/src/main/java/calorietracker/models/NutriInfo.java
@@ -0,0 +1,65 @@
+package calorietracker.models;
+
+public class NutriInfo extends Model {
+    String nama;
+    int energi, berat;
+    double protein, lemak, karbohidrat;
+
+    public NutriInfo(int id, String nama, int energi, double protein, double lemak, double karbohidrat,int berat) {
+        super(id);
+        this.nama = nama;
+        this.energi = energi;
+        this.protein = protein;
+        this.lemak = lemak;
+        this.karbohidrat = karbohidrat;
+        this.berat = berat;
+    }
+
+    public String getNama() {
+        return nama;
+    }
+
+    public void setNama(String nama) {
+        this.nama = nama;
+    }
+
+    public int getEnergi() {
+        return energi;
+    }
+
+    public void setEnergi(int energi) {
+        this.energi = energi;
+    }
+
+    public int getBerat() {
+        return berat;
+    }
+
+    public void setBerat(int berat) {
+        this.berat = berat;
+    }
+
+    public double getProtein() {
+        return protein;
+    }
+
+    public void setProtein(double protein) {
+        this.protein = protein;
+    }
+
+    public double getLemak() {
+        return lemak;
+    }
+
+    public void setLemak(double lemak) {
+        this.lemak = lemak;
+    }
+
+    public double getKarbohidrat() {
+        return karbohidrat;
+    }
+
+    public void setKarbohidrat(double karbohidrat) {
+        this.karbohidrat = karbohidrat;
+    }
+}
